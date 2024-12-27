f636ea37017a7351f9dfcd0c55dd2181c3097c76
Esha Singh Bhardwaj <105241309+singhesharajput@users.noreply.github.com>
2024-12-27 10:56:21 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -2,22 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/userRouter.js';
 import { DBConnect } from './dbConfig/dbConfig.js';
-
-
+import { CLIENT_URL } from './constants.js';
 
 DBConnect();
 const app = express();
 
-
 app.use(express.json());
 app.use("/api/users", router);
-
-app.use(cors({
-    origin: 'http://localhost:3000', // Frontend URL
-    methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
-    allowedHeaders: '*', // Allowed headers
-    credentials: true, // Allow cookies/credentials
-  }));
 
 app.get('/api', (req, res) => {
     res.send('Server is running');
@@ -29,3 +20,11 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
 });
+
+const corsOptions = {
+    origin: CLIENT_URL,  // Allow only this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,10 +3,37 @@ import { createUser, getAllUsers, deleteUser, login } from "../controllers/userC
 
 const router = Router();
 
+// ! user
 router.post("/createUser", createUser);
 router.get('/getAllUsers', getAllUsers);
 router.delete('/deleteUser/:id', deleteUser);
+
+// ! login
 router.post('/login', login);
 
+// ! destination
+router.post("/createDestination", createUser);
+router.get('/getAllDestinations', getAllUsers);
+router.delete('/deleteDestination/:id', deleteUser);
+
+// ! trip
+router.post("/createTrip", createUser);
+router.get('/getAllTrips', getAllUsers);
+router.delete('/deleteTrip/:id', deleteUser);
+
+// ! booking
+router.post("/createBooking", createUser);
+router.get('/getAllBookings', getAllUsers);
+router.delete('/deleteBooking/:id', deleteUser);
+
+// ! agent
+router.post("/createAgent", createUser);
+router.get('/getAllAgents', getAllUsers);
+router.delete('/deleteAgent/:id', deleteUser);
+
+// ! supplier 
+router.post("/createSupplier", createUser);
+router.get('/getAllSuppliers', getAllUsers);
+router.delete('/deleteSupplier/:id', deleteUser);
 
 export default router;
