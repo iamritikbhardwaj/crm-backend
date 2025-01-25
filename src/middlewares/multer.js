ade3b627e73b49265e7fe0a14da154f02ee52e70
Ritik Singh <ritiklrt2@gmail.com>
2025-01-25 16:20:41 +0530

    build fix

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,7 +1,7 @@
 import multer from 'multer';
 import fs from 'fs';
 
-const tempDir = `public/temp/${Date.now()}`;
+const tempDir = `public/temp`;
 if (!fs.existsSync(tempDir)) {
     fs.mkdirSync(tempDir, { recursive: true });
 }
