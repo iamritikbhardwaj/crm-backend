e326710bd580871d29a5caf4ce1a7a91a4050f15
Ritik Singh <ritiklrt2@gmail.com>
2025-03-31 15:59:44 +0530

    build for filter visibility fix

---
+++ src/utils/updateStatus.util.js
@@ -0,0 +1,31 @@
+import { Op } from 'sequelize';
+import sequelize from '../dbConfig/dbConfig.js';
+import { tripModel } from '../models/tripModel.js';
+
+// Function to update booking status based on dates
+async function updateTripStatus() {
+  try {
+    const updatedRows = await tripModel.update(
+      {
+        status: sequelize.Sequelize.literal(`
+          CASE 
+            WHEN arrivalDate <= CURDATE() AND status != 'CANCELLED' THEN 'ON-TOUR' 
+            WHEN departureDate <= CURDATE() AND status != 'CANCELLED' THEN 'TRAVELLED' 
+            ELSE status 
+          END
+        `)
+      },
+      {
+        where: {
+          status: { [Op.ne]: 'CANCELLED' } // Ensure the status is not 'CANCELLED'
+        }
+      }
+    );
+
+    console.log(`${updatedRows[0]} bookings updated successfully.`);
+  } catch (error) {
+    console.error('Error updating booking status:', error);
+  }
+}
+
+export default updateTripStatus;
