4939daadcc055aa6503e44b455a590aa3374f2da
Ritik Singh <ritiklrt2@gmail.com>
2025-04-02 11:21:32 +0530

    hide edit

--- src/index.js
+++ src/index.js
@@ -30,8 +30,6 @@ app.use(cors(corsOptions));
 
 app.use(cookieParser());
 
-app.use(express.json({ limit: '50mb' }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", router);
